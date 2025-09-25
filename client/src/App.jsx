9185a182fd10c4d1aa0429c10a5c7649ca4a4486
Jesafathima <jazarf24@gmail.com>
2025-09-25 22:32:01 +0530

    add course details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register'; 
+import CourseDetails from './pages/Coursedetails';
 //import CreateCourse from './pages/CreateCourse';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />} />
+        <Route path ="/courses/:id" element={<CourseDetails />} />
       </Routes>
     </Router>
   );
---
+++ client/src/pages/CourseDetails.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../services/api";
+//import Loading from "../components/Loading";
+
+const CourseDetails =() => {
+    const { id } = useParams();
+    const [course, setCourse] = useState (null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+    const fetchCourse = async () => {
+      try {
+        const response = await api.get(`/courses/${id}`);
+        setCourse(response.data);
+      } catch (err) {
+        console.error("Error fetching course:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCourse();
+  }, [id]);
+
+  if (loading) return <Loading />;
+
+  if (!course) return <p>Course not found.</p>;
+
+  return (
+    <div className="p-6 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4">{course.title}</h1>
+      <p className="text-gray-700 mb-4">{course.description}</p>
+
+      {/* If mediaUrl is a video */}
+      {course.mediaUrl && (
+        <video
+          src={course.mediaUrl}
+          controls
+          className="w-full rounded-lg mb-4"
+        />
+      )}
+
+      <div className="prose max-w-none">
+        {course.content.split("\n").map((line, index) => (
+          <p key={index}>{line}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CourseDetails;
