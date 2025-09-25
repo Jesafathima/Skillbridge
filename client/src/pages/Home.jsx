ab75e17cf890c8a0b45d69083079f2d56002a3fa
Jesafathima <jazarf24@gmail.com>
2025-09-25 17:34:08 +0530

    add hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import CourseCard from "../components/CourseCard"; // reusable card component
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import heroImage from "../assets/Hero_Image.jpeg"; 
 
 
 const Home = () => {
@@ -32,7 +33,28 @@ const Home = () => {
       {/* Navigation bar Section */}
       <Navbar/>
 
-     
+      {/* Hero Section */}
+      <section className="relative">
+        <img
+          src={heroImage}
+          alt="SkillBridge Banner"
+          className="w-full h-96 object-cover"
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
+          <h1 className="text-4xl sm:text-5xl font-bold text-white">
+            Update your skills without limits
+          </h1>
+        </div>
+        <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
+          <h1 className="text-4xl sm:text-5xl font-bold text-white">
+            Learn. Build. Share.
+          </h1>
+        </div>
+        <button className="absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700 transition">
+          Explore Courses
+        </button>
+      </section>
+
       {/* Courses Section */}
       <h1 >Available Courses</h1>
       {courses.length > 0 ? (
