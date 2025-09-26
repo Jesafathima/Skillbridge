2a1ff9af19a739b69ff8c5daaef9658dfcd81739
Jesafathima <jazarf24@gmail.com>
2025-09-26 10:53:12 +0530

    backend server setup

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("SkillBridge API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
